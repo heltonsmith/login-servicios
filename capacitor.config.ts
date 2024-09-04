2a2d9f74a87b8ce31e41ad07b737f08941586d52
Helton Bustos <heltonsmith@hotmail.com>
2024-09-03 23:23:53 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'login-servicios',
+  webDir: 'www'
+};
+
+export default config;
